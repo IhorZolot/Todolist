08d967a762d3d9c3374d175e9c64cdf31e7f2a48
Ihor <fedonn@ukr.net>
2023-09-01 20:42:41 +0300

    toggleTodo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { AddForm } from './components/AddForm'
 import { Todo } from './components/Todo'
+import axios from 'axios'
 
-const date = [
-	{ title: 'todo1', id: 1, complited: true },
-	{ title: 'todo2', id: 2, complited: false },
-]
+const date = []
 
 function App() {
 	const [todo, setTodo] = useState(date)
+	useEffect(() => {
+		axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5').then(res => {
+			console.log(res)
+			setTodo(res.data)
+		})
+	}, [])
+	const toggleTodo = id => {
+		setTodo(prev => prev.map(item => (item.id === id ? { ...item, completed: !item.completed } : item)))
+	}
 
 	const deliteTodo = id => {
 		setTodo(todo.filter(item => item.id !== id))
@@ -20,7 +27,7 @@ function App() {
 	return (
 		<div>
 			<AddForm addtodo={addtodo} />
-			<Todo deliteTodo={deliteTodo} todos={todo} />
+			<Todo deliteTodo={deliteTodo} todos={todo} toggleTodo={toggleTodo} />
 		</div>
 	)
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-export const Todo = ({ todos = [], deliteTodo }) => {
+export const Todo = ({ todos = [], deliteTodo, toggleTodo }) => {
 	return (
 		<div>
 			<ul>
-				{todos.map(({ id, title }) => (
+				{todos.map(({ id, title, completed }) => (
 					<li key={id}>
 						{' '}
+						<input type='checkbox' onChange={() => toggleTodo(id)} checked={completed}></input>
 						{title}
 						<button onClick={() => deliteTodo(id)}>DelTodo</button>
 					</li>
